134cd4aca4f9b250d683eae1f25bf95bc76c51d6
Sacamoto84 <zorromen@mail.ru>
2025-05-18 21:34:20 +0300

    49

--- app/src/main/java/com/client/xvideos/screens_red/profile/ScreenRedProfile.kt
+++ app/src/main/java/com/client/xvideos/screens_red/profile/ScreenRedProfile.kt
@@ -2,25 +2,21 @@ package com.client.xvideos.screens_red.profile
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
@@ -28,12 +24,8 @@ import cafe.adriel.voyager.core.screen.uniqueScreenKey
 import cafe.adriel.voyager.hilt.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import com.client.xvideos.feature.redgifs.types.MediaInfo
-import com.client.xvideos.screens.common.urlVideImage.UrlImage
 import com.client.xvideos.screens_red.ThemeRed
-import com.client.xvideos.screens_red.profile.atom.RedProfileCreaterInfo
-import com.client.xvideos.screens_red.profile.atom.toMinSec
-import com.client.xvideos.screens_red.profile.atom.toPrettyCount
+import com.client.xvideos.screens_red.profile.atom.RedProfileTile
 import com.composables.core.HorizontalSeparator
 
 class ScreenRedProfile() : Screen {
@@ -52,64 +44,51 @@ class ScreenRedProfile() : Screen {
         Scaffold(
             bottomBar = { RedBottomBar() },
             containerColor = ThemeRed.colorCommonBackground
-        ) {
-
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-
-                item {
-                    if (vm.b != null) {
-                        RedProfileCreaterInfo(vm.b!!)
-                    }
-                }
+        ) {padding ->
 
 
 
-                items(items = list.orEmpty(), key = { it.id }) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.padding(padding).fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                contentPadding = PaddingValues(4.dp) // Отступы по краям сетки
+            ) {
 
+                items(items = list.orEmpty(), key = { it.id }) {itemData ->
                     Box(
                         modifier = Modifier
-                            .aspectRatio(1f)
-                            .clip(RoundedCornerShape(8.dp))
                     ) {
-
-                        UrlImage(
-                            url = it.urls.thumbnail,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .fillMaxSize()
-                        )
-
-                        Text(
-                            it.views?.toPrettyCount() ?: "-",
-                            color = Color.White,
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .align(androidx.compose.ui.Alignment.BottomStart),
-                            fontFamily = ThemeRed.fontFamilyPopinsMedium
-                        )
-
-
-                        Text(
-                            it.duration?.toMinSec() ?: "-",
-                            color = Color.White,
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .align(androidx.compose.ui.Alignment.BottomEnd),
-                            fontFamily = ThemeRed.fontFamilyPopinsMedium
-                        )
-
-
+                        RedProfileTile(itemData)
                     }
-
-
                 }
-
-
             }
 
 
-        }
+//            LazyColumn(modifier = Modifier.fillMaxSize()) {
+//
+////                item {
+////                    if (vm.b != null) {
+////                        RedProfileCreaterInfo(vm.b!!)
+////                    }
+////                }
+//                item {
+//
+//
+//
+//                }
+//
+//
+////                items(items = list.orEmpty(), key = { it.id }) {
+////                    RedProfileTile(it)
+////                }
+//
+//            }
 
+
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/client/xvideos/screens_red/profile/atom/RedProfileTile.kt
@@ -0,0 +1,114 @@
+package com.client.xvideos.screens_red.profile.atom
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.client.xvideos.R
+import com.client.xvideos.feature.redgifs.types.MediaInfo
+import com.client.xvideos.screens.common.urlVideImage.UrlImage
+import com.client.xvideos.screens_red.ThemeRed
+import com.composables.core.Icon
+
+@Composable
+fun RedProfileTile(item : MediaInfo) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+
+        UrlImage(
+            url = item.urls.thumbnail,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Row(
+                modifier = Modifier.padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Box{
+                    Icon(
+                        painter = painterResource(R.drawable.rg_button),
+                        contentDescription = null,
+                        tint = Color.Black
+                        , modifier = Modifier.offset(1.dp, 1.dp)
+                    )
+                    Icon(
+                        painter = painterResource(R.drawable.rg_button),
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+
+                Box {
+                    Text(
+                        item.views?.toPrettyCount() ?: "-",
+                        color = Color.Black,
+                        modifier = Modifier
+                            .padding(start = 8.dp).offset(1.dp, 1.dp),
+                        fontFamily = ThemeRed.fontFamilyPopinsMedium
+                    )
+
+                    Text(
+                        item.views?.toPrettyCount() ?: "-",
+                        color = Color.White,
+                        modifier = Modifier
+                            .padding(start = 8.dp),
+                        fontFamily = ThemeRed.fontFamilyPopinsMedium
+                    )
+                }
+
+            }
+
+
+            Box {
+
+                Text(
+                    item.duration?.toMinSec() ?: "-",
+                    color = Color.Black,
+                    modifier = Modifier
+                        .padding(8.dp).offset(1.dp, 1.dp),
+                    fontFamily = ThemeRed.fontFamilyPopinsMedium
+                )
+
+                Text(
+                    item.duration?.toMinSec() ?: "-",
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(8.dp),
+                    fontFamily = ThemeRed.fontFamilyPopinsMedium
+                )
+            }
+        }
+
+    }
+
+}
