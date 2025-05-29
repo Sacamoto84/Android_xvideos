fccb79a1c3a7866ebeff1f8707fc59f4bc7dbbcd
Sacamoto84 <zorromen@mail.ru>
2025-05-29 23:15:56 +0300

    86

--- app/src/main/java/com/client/xvideos/screens_red/common/VideoPlayerWithMenuContent.kt
+++ app/src/main/java/com/client/xvideos/screens_red/common/VideoPlayerWithMenuContent.kt
@@ -1,4 +1,4 @@
-package com.client.xvideos.feature.videoplayer.chaintech.videoplayer.ui.video
+package com.client.xvideos.screens_red.common
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.core.tween
@@ -27,7 +27,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -42,7 +41,6 @@ import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.client.xvideos.BuildConfig
 import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.host.MediaPlayerHost
-import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.model.PlayerOption
 import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.model.VideoPlayerConfig
 import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.util.CMPPlayer2
 import net.engawapg.lib.zoomable.rememberZoomState
@@ -57,7 +55,6 @@ import kotlin.math.roundToInt
 fun VideoPlayerWithMenuContent(
     modifier: Modifier,
     playerHost: MediaPlayerHost,
-    playerConfig: VideoPlayerConfig,
     onClick: () -> Unit = {},
 
     menuContent: @Composable () -> Unit = {},
@@ -96,98 +93,20 @@ fun VideoPlayerWithMenuContent(
 
     Box( modifier = modifier.clipToBounds() ) {
 
-        Box(
-            modifier = modifier
-                .zoomable(
-                    zoomState = zoomState,
-                    zoomEnabled = (playerConfig.isZoomEnabled),
-                    enableOneFingerZoom = false,
-                    onTap = {
-                        onClick.invoke()
-                    }
-                )
-        ) {
-
+        Box(modifier = modifier.zoomable(zoomState = zoomState, zoomEnabled = true, enableOneFingerZoom = false, onTap = { onClick.invoke() })) {
             StaticPlayer( playerHost, autoRotate )
-
-
-
-                //--- Меню контент ---
-                BoxWithConstraints(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                )
-                {
-                    val width = maxWidth
-                    val maxWidthPx = with(density) { (width).toPx() }
-                    val startPx = with(density) { (width - squareSize).toPx() }
-
-                    val anchors = DraggableAnchors {
-                        SwipeState.Center at startPx
-                        SwipeState.Right at maxWidthPx
-                    }
-
-                    val swipeState = remember { AnchoredDraggableState(initialValue = if (menuDefaultOpen) SwipeState.Center else SwipeState.Right, anchors = anchors)}
-                    LaunchedEffect(swipeState.currentValue) {
-                        Timber.i("@@@ LaunchedEffect(swipeState)")
-                        menuOpenChanged(swipeState.currentValue == SwipeState.Center)
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .padding(top = 100.dp).height(64.dp).fillMaxWidth()
-                            .anchoredDraggable(
-                                swipeState, Orientation.Horizontal,
-                                flingBehavior =
-                                    AnchoredDraggableDefaults.flingBehavior(swipeState, positionalThreshold = { distance -> distance * 0.125f }, animationSpec = tween(100))
-                            ), contentAlignment = Alignment.CenterStart
-                    ) {
-
-                        if (swipeState.currentValue == SwipeState.Right && !swipeState.isAnimationRunning) {
-                            Box(Modifier.width(2.dp).height(48.dp).align(Alignment.CenterEnd).alpha(0.6f).background(color = Color(0xFFFFFFFF),shape = RoundedCornerShape(1.dp)))
-                        }
-
-                        Box(
-                            modifier = Modifier
-                                .offset {IntOffset(x = swipeState.requireOffset().roundToInt(), y = 0)}
-                                .width(squareSize).height(64.dp).background(Color(0xA1969696), shape = RoundedCornerShape(topStart = 12.dp, bottomStart = 12.dp)),
-                            contentAlignment = Alignment.CenterStart
-                        ) {
-                            menuContent.invoke()
-                        }
-
-                    }
-
-                }
-                //!!!--- Меню контент ---!!!
-
-
         }
+
+        //--- Меню контент ---
+        MenuContent(menuContent = menuContent, menuContentWidth=menuContentWidth, menuDefaultOpen=menuDefaultOpen, menuOpenChanged=menuOpenChanged)
 
         //Нижняя сенсорная часть
         Box(modifier = Modifier.fillMaxHeight(1 / 3f).fillMaxWidth().align(Alignment.BottomCenter).then(volumeDragModifier).alpha(0.5f).background(Color.Transparent))
 
-
         if (playerHost.isBuffering) {
-
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                if (playerConfig.loaderView != null) {
-                    playerConfig.loaderView?.invoke()
-                } else {
-
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .size(playerConfig.pauseResumeIconSize),
-                        color = playerConfig.loadingIndicatorColor
-                    )
-
-                }
+            Box( modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center ) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center).size(40.dp), color = Color.LightGray)
             }
-
         }
 
     }
@@ -196,6 +115,68 @@ fun VideoPlayerWithMenuContent(
 
 
 
+
+@SuppressLint("UnusedBoxWithConstraintsScope")
+@Composable
+private fun MenuContent(
+    menuContent: @Composable () -> Unit = {},
+    menuContentWidth: Dp = 192.dp,
+    menuDefaultOpen: Boolean,
+    menuOpenChanged: (Boolean) -> Unit){
+
+    val squareSize = menuContentWidth
+    val density = LocalDensity.current
+
+    //--- Меню контент ---
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    )
+    {
+        val width = maxWidth
+        val maxWidthPx = with(density) { (width).toPx() }
+        val startPx = with(density) { (width - squareSize).toPx() }
+
+        val anchors = DraggableAnchors {
+            SwipeState.Center at startPx
+            SwipeState.Right at maxWidthPx
+        }
+
+        val swipeState = remember { AnchoredDraggableState(initialValue = if (menuDefaultOpen) SwipeState.Center else SwipeState.Right, anchors = anchors)}
+        LaunchedEffect(swipeState.currentValue) {
+            Timber.i("@@@ LaunchedEffect(swipeState)")
+            menuOpenChanged(swipeState.currentValue == SwipeState.Center)
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(top = 100.dp).height(64.dp).fillMaxWidth()
+                .anchoredDraggable(
+                    swipeState, Orientation.Horizontal,
+                    flingBehavior =
+                        AnchoredDraggableDefaults.flingBehavior(swipeState, positionalThreshold = { distance -> distance * 0.125f }, animationSpec = tween(100))
+                ), contentAlignment = Alignment.CenterStart
+        ) {
+
+            if (swipeState.currentValue == SwipeState.Right && !swipeState.isAnimationRunning) {
+                Box(Modifier.width(2.dp).height(48.dp).align(Alignment.CenterEnd).alpha(0.6f).background(color = Color(0xFFFFFFFF),shape = RoundedCornerShape(1.dp)))
+            }
+
+            Box(
+                modifier = Modifier
+                    .offset {IntOffset(x = swipeState.requireOffset().roundToInt(), y = 0)}
+                    .width(squareSize).height(64.dp).background(Color(0xA1969696), shape = RoundedCornerShape(topStart = 12.dp, bottomStart = 12.dp)),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                menuContent.invoke()
+            }
+
+        }
+
+    }
+    //!!!--- Меню контент ---!!!
+
+}
 
 @Composable
 private fun StaticPlayer(
@@ -236,11 +217,5 @@ autoRotate: Boolean
     )
 
 }
-
-
-
-
-
-
 
 enum class SwipeState { Left, Center, Right }
--- app/src/main/java/com/client/xvideos/screens_red/profile/atom/RedUrlVideoLiteChaintech.kt
+++ app/src/main/java/com/client/xvideos/screens_red/profile/atom/RedUrlVideoLiteChaintech.kt
@@ -2,7 +2,6 @@ package com.client.xvideos.screens_red.profile.atom
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,23 +17,16 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.request.CachePolicy
-import coil.request.ImageRequest
 import com.client.xvideos.BuildConfig
 import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.host.MediaPlayerEvent
 import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.host.MediaPlayerHost
 import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.model.ScreenResize
-import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.model.VideoPlayerConfig
-import com.client.xvideos.feature.videoplayer.chaintech.videoplayer.ui.video.VideoPlayerWithMenuContent
 import com.client.xvideos.screens_red.ThemeRed
+import com.client.xvideos.screens_red.common.VideoPlayerWithMenuContent
 import com.client.xvideos.screens_red.profile.PlayerControls
-import com.skydoves.landscapist.ImageOptions
-import com.skydoves.landscapist.coil.CoilImage
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
@@ -62,21 +54,14 @@ fun RedUrlVideoLiteChaintech(
     autoRotate: Boolean
 ) {
 
-    if (BuildConfig.DEBUG) {
-        SideEffect {
-            Timber.i("@@@ RedUrlVideoLiteChaintech() play:$play isMute:$isMute timeA:$timeA menuDefaultOpen:$menuDefaultOpen")
-        }
-    }
+    if (BuildConfig.DEBUG) { SideEffect { Timber.i("@@@ RedUrlVideoLiteChaintech() play:$play isMute:$isMute timeA:$timeA menuDefaultOpen:$menuDefaultOpen") } }
 
     val scope = rememberCoroutineScope()
 
     var currentTime by remember { mutableFloatStateOf(0f) }
     var duration by remember { mutableIntStateOf(0) }
 
-    LaunchedEffect(Unit) {
-        snapshotFlow { currentTime to duration }
-            .collectLatest { onChangeTime(it) }
-    }
+    LaunchedEffect(Unit) { snapshotFlow { currentTime to duration }.collectLatest { onChangeTime(it) } }
 
     val playerHost = remember { MediaPlayerHost(mediaUrl = url, isPaused = true) }
 
@@ -86,80 +71,16 @@ fun RedUrlVideoLiteChaintech(
         playerHost.seekTo(0f)
     }
 
-    LaunchedEffect(play) {
-        if (play)
-            playerHost.play()
-        else
-            playerHost.pause()
-    }
+    LaunchedEffect(enableAB) { if (enableAB) { playerHost.seekTo(timeA.toFloat()) } }
 
-    LaunchedEffect(playerHost, onPlayerControlsReady) {
-        val controls = object : PlayerControls {
+    LaunchedEffect(play) { if (play) playerHost.play() else playerHost.pause() }
 
-            override fun forward(seconds: Float) {
-                val currentPosition =
-                    (currentTime + seconds).coerceAtMost(duration.toFloat()).toFloat()
-                playerHost.seekTo(currentPosition)
-            }
+    LaunchedEffect(isMute) { if (isMute) playerHost.mute() else playerHost.unmute() }
 
-            override fun rewind(seconds: Float) {
-                val currentPosition = (currentTime - seconds).coerceAtLeast(0f).toFloat()
-                playerHost.seekTo(currentPosition)
-            }
-
-            override fun seekTo(positionSeconds: Float) {
-                playerHost.seekTo(
-                    positionSeconds.coerceIn(0f, duration.toFloat()).toFloat()
-                )
-            }
-
-            override fun stop() {
-                playerHost.pause()
-                playerHost.seekTo(0f)
-            }
-
-            override fun pause() {
-                playerHost.pause()
-            }
-
-            override fun play() {
-                playerHost.play()
-            }
-        }
-        onPlayerControlsReady(controls)
-    }
-
-    Box(modifier = Modifier.fillMaxWidth()) {
-
-        LaunchedEffect(isMute) {
-            if (isMute)
-                playerHost.mute()
-            else
-                playerHost.unmute()
-        }
-
-//        LaunchedEffect(currentTime, duration) {
-//            Timber.d("@@@ RedUrlVideoLiteChaintech onChangeTime($currentTime, $duration)")
-//
-//            //onChangeTime(currentTime to duration)
-//        }
-
-
+    LaunchedEffect(playerHost) {
+        playerHost.setVideoFitMode(ScreenResize.FIT)
         playerHost.onEvent = { event ->
             when (event) {
-                is MediaPlayerEvent.MuteChange -> {
-                    println("!!! Mute status changed: ${event.isMuted}")
-                }
-
-                is MediaPlayerEvent.PauseChange -> {
-                    println("!!!Pause status changed: ${event.isPaused}")
-                }
-
-                is MediaPlayerEvent.BufferChange -> {
-                    println("???Buffering status: ${event.isBuffering}")
-                    //isBuffering = event.isBuffering
-                }
-
                 is MediaPlayerEvent.CurrentTimeChange -> {
                     //println("!!!Current playback time: ${event.currentTime}s")
                     currentTime = event.currentTime
@@ -170,76 +91,38 @@ fun RedUrlVideoLiteChaintech(
                         }
                     }
                 }
-
-                is MediaPlayerEvent.TotalTimeChange -> {
-                    println("!!!Video duration updated: ${event.totalTime}s")
-                    duration = event.totalTime.toInt()
-                }
-
-                is MediaPlayerEvent.FullScreenChange -> {
-                    println("!!!FullScreen status changed: ${event.isFullScreen}")
-                }
-
-                MediaPlayerEvent.MediaEnd -> {
-                    println("!!!Video playback ended")
-                }
+                is MediaPlayerEvent.TotalTimeChange -> { println("!!!Video duration updated: ${event.totalTime}s"); duration = event.totalTime.toInt() }
+                else -> {}
             }
         }
+    }
 
-        LaunchedEffect(enableAB) {
-            if (enableAB) {
-                playerHost.seekTo(timeA.toFloat())
-            } else {
-                //playerHost.seekTo(0f)
-            }
+    LaunchedEffect(playerHost, onPlayerControlsReady) {
+
+        val controls = object : PlayerControls {
+            override fun forward(seconds: Float) { val currentPosition = (currentTime + seconds).coerceAtMost(duration.toFloat()).toFloat(); playerHost.seekTo(currentPosition) }
+            override fun rewind(seconds: Float) { val currentPosition = (currentTime - seconds).coerceAtLeast(0f).toFloat(); playerHost.seekTo(currentPosition) }
+            override fun seekTo(positionSeconds: Float) { playerHost.seekTo( positionSeconds.coerceIn(0f, duration.toFloat()).toFloat()) }
+            override fun stop()  { playerHost.pause(); playerHost.seekTo(0f) }
+            override fun pause() { playerHost.pause() }
+            override fun play()  { playerHost.play() }
         }
 
-        playerHost.setVideoFitMode(ScreenResize.FIT)
-
-
-        VideoPlayerWithMenuContent(
-            onClick = onClick,
-            modifier = Modifier.fillMaxSize(),
-            playerHost = playerHost,
-            playerConfig = VideoPlayerConfig(
-                isPauseResumeEnabled = true,
-                isSeekBarVisible = true,
-                isMuteControlEnabled = true,
-                isDurationVisible = true,
-                seekBarThumbColor = Color.Red,
-                seekBarActiveTrackColor = Color.Red,
-                seekBarInactiveTrackColor = Color.White,
-                durationTextColor = Color.White,
-                seekBarBottomPadding = 10.dp,
-                pauseResumeIconSize = 40.dp,
-                isAutoHideControlEnabled = true,
-                controlHideIntervalSeconds = 5,
-                isFastForwardBackwardEnabled = true,
-                isGestureVolumeControlEnabled = true,
-                showAudioTracksOptions = false,
-                showControls = true,
-
-                loaderView = {
-
-//                    UrlImage1(
-//                        url = thumnailUrl,
-//                        contentScale = ContentScale.Fit,
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .background(Color.Transparent)
-//                        //.zIndex(1f) // убедиться, что над видео
-//                    )
-
-                },
-            ),
-            menuContent = menuContent,
-            menuContentWidth = menuContentWidth,
-            menuDefaultOpen = menuDefaultOpen,
-            menuOpenChanged = menuOpenChanged,
-            autoRotate = autoRotate
-        )
+        onPlayerControlsReady(controls)
 
     }
+
+    VideoPlayerWithMenuContent(
+        onClick = onClick,
+        modifier = Modifier.fillMaxSize(),
+        playerHost = playerHost,
+
+        menuContent = menuContent,
+        menuContentWidth = menuContentWidth,
+        menuDefaultOpen = menuDefaultOpen,
+        menuOpenChanged = menuOpenChanged,
+        autoRotate = autoRotate
+    )
 
 }
 
@@ -262,40 +145,6 @@ fun CurrentTimeText(
     }
 }
 
-@Composable
-fun UrlImage1(
-    url: String,
-    modifier: Modifier = Modifier,
-    contentScale: ContentScale = ContentScale.FillWidth,
-) {
-
-    val context = LocalContext.current
-
-    val imageRequest = ImageRequest.Builder(context)
-        .data(url)
-        .crossfade(true)
-        .memoryCacheKey(url)
-        .diskCacheKey(url)
-        .diskCachePolicy(CachePolicy.ENABLED)
-        .memoryCachePolicy(CachePolicy.ENABLED)
-        .build()
-
-    CoilImage(
-        imageRequest = { imageRequest },
-        imageOptions = ImageOptions(
-            contentScale = contentScale,
-            alignment = Alignment.Center
-        ),
-        modifier = Modifier.then(modifier),
-
-        failure = {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                com.composeunstyled.Text("Ошибка загрузки", color = Color.Gray)
-            }
-        }
-
-    )
-}
 
 // Вспомогательная функция для форматирования времени из секунд в MM:SS
 private fun formatDuration(totalSeconds: Int): String {
