adf395fac6bb6d84de6cb6b63d3960d059b0cf15
Sacamoto84 <zorromen@mail.ru>
2025-08-15 19:58:39 +0300

    149.2

--- app/src/main/java/com/client/xvideos/l/model/ContentTypeOptions.kt
+++ app/src/main/java/com/client/xvideos/l/model/ContentTypeOptions.kt
@@ -1,5 +1,15 @@
 package com.client.xvideos.l.model
 
+/**
+ * Группа filters
+ *
+ * { name  : "content_id", value :  "2" }
+ *
+ * All 0
+ * Hentai "2"
+ * Non-Erotic 5
+ * Real People 6
+ */
 enum class ContentTypeOptions(val value: Int) {
     All(0),
     Hentai(2),
---
+++ app/src/main/java/com/client/xvideos/l/model/content_id.kt
@@ -0,0 +1,16 @@
+package com.client.xvideos.l.model
+
+
+
+/**
+ * "album_type"
+ *
+ * Picture Sets "pictures"
+ *
+ * Manga "manga"
+ *
+ * All отсуствие в фильтре
+ *
+ */
+
+
