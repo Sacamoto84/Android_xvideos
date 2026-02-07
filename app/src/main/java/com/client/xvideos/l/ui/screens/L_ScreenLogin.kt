113d419c9ce89c782ac51f667dd5441752fcb285
Sacamoto84 <zorromen@mail.ru>
2026-02-07 22:29:58 +0300

    155.7.47

--- app/src/main/java/com/client/xvideos/l/ui/screens/L_ScreenLogin.kt
+++ app/src/main/java/com/client/xvideos/l/ui/screens/L_ScreenLogin.kt
@@ -32,9 +32,7 @@ private fun PContent() {
     var password by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
